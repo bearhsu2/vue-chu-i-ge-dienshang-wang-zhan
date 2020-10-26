c84b673b81c6d05c2e9c9c5f9b5ec306f5887c30
Yu-Song Syu <kuma@simbo.com.tw>
2020-10-27 00:13:11 +0800

    add router-links

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <template>
     <div id="app">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Navbar</a>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+
+                    <li class="nav-item active">
+                        <router-link class="nav-link" to='/'>Home</router-link>
+                    </li>
+                    <li class="nav-item active">
+                        <router-link class="nav-link" to='/page'>Page</router-link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
         <img alt="Vue logo" src="./assets/logo.png">
-<!--        <HelloWorld msg="Welcome to Your Vue.js App"/>-->
 
         <router-view></router-view>
 
@@ -9,12 +23,10 @@
 </template>
 
 <script>
-    import HelloWorld from './components/HelloWorld.vue'
 
     export default {
         name: 'App',
-        components: {
-        },
+        components: {},
         created() {
             this.$http.get('https://randomuser.me/api/')
                 .then((response) => {
@@ -25,14 +37,14 @@
 </script>
 
 <style lang="scss">
-    #app {
-      font-family: Avenir, Helvetica, Arial, sans-serif;
-      -webkit-font-smoothing: antialiased;
-      -moz-osx-font-smoothing: grayscale;
-      text-align: center;
-      color: #2c3e50;
-      margin-top: 60px;
-    }
+    /*#app {*/
+    /*    font-family: Avenir, Helvetica, Arial, sans-serif;*/
+    /*    -webkit-font-smoothing: antialiased;*/
+    /*    -moz-osx-font-smoothing: grayscale;*/
+    /*    text-align: center;*/
+    /*    color: #2c3e50;*/
+    /*    margin-top: 60px;*/
+    /*}*/
 
 
 </style>
