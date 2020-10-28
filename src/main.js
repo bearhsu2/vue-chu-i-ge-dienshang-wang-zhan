6d695876794291ab7dd8c975d7ed1fef5eb1c494
Yu-Song Syu <kuma@simbo.com.tw>
2020-10-28 17:40:26 +0800

    import third-party library before ours

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
-import App from '@/App' // ".vue" can be ignored
 import axios from 'axios'
 import VueAxios from 'vue-axios';
+import App from '@/App' // ".vue" can be ignored
 import router from '@/router';
+
 
 Vue.use(VueAxios, axios); //啟用套件
 Vue.config.productionTip = false;
