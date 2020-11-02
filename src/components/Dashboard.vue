14641245ad430b62a8986cf45486779b1e7a4b88
Yu-Song Syu <kuma@simbo.com.tw>
2020-11-02 22:51:37 +0800

    extract navbar to component

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,22 +2,7 @@
     <div>
 
 
-        <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-            <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">Company name</a>
-            <button aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation"
-                    class="navbar-toggler position-absolute d-md-none collapsed" data-target="#sidebarMenu"
-                    data-toggle="collapse"
-                    type="button">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <input aria-label="Search" class="form-control form-control-dark w-100" placeholder="Search" type="text">
-            <ul class="navbar-nav px-3">
-                <li class="nav-item text-nowrap">
-                    <a class="nav-link" href="#">Sign out</a>
-                </li>
-            </ul>
-        </nav>
-
+        <Navbar></Navbar>
         <div class="container-fluid">
             <div class="row">
                 <nav class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse" id="sidebarMenu">
@@ -97,7 +82,7 @@
                 </nav>
 
                 <main class="col-md-9 ml-sm-auto col-lg-10 px-md-4" role="main">
-                  <router-view></router-view>
+                    <router-view></router-view>
                 </main>
             </div>
 
@@ -107,8 +92,10 @@
 </template>
 
 <script>
+    import Navbar from './Navbar';
+
     export default {
-        components: {}
+        components: {Navbar}
     }
 
 </script>
