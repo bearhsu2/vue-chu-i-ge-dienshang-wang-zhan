fd501b78b7bee1ddbfb6cb2fe8770b8b1228bd8a
Yu-Song Syu <kuma@simbo.com.tw>
2020-11-06 00:17:59 +0800

    put products to table

--- src/components/pages/Products.vue
+++ src/components/pages/Products.vue
@@ -1,8 +1,37 @@
 <template>
     <div>
         <div class="text-right mt-4">
-            <button class = "btn btn-primary">新增產品</button>
+            <button class="btn btn-primary">新增產品</button>
         </div>
+
+
+        <table class="table table-striped mt-4">
+            <thead>
+            <td>分類</td>
+            <td>產品名稱</td>
+            <td>原價</td>
+            <td>售價</td>
+            <td>啟用</td>
+            <td>編輯</td>
+            </thead>
+            <tbody>
+            <tr :key=item.id v-for="item in products">
+
+                <td>{{item.category}}</td>
+                <td>{{item.title}}</td>
+                <td>{{item.origin_price}}</td>
+                <td>{{item.price}}</td>
+                <td>
+                    <span class="text-success" v-if="item.is_enabled">Active</span>
+                    <span v-else>Inactive</span>
+                </td>
+                <td>
+                    <button class="btn btn-outline-primary btn-sm">編輯</button>
+                </td>
+
+            </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 <script>
@@ -18,8 +47,9 @@
         methods: {
             getProducts() {
                 const vm = this;
-                this.$http.get('https://vue-course-api.hexschool.io/api/bearhsu2/products')
+                this.$http.get('http://vue-course-api.hexschool.io/api/bearhsu2/products')
                     .then((response) => {
+                        console.log(response);
                         vm.products = response.data.products;
                     })
             }
