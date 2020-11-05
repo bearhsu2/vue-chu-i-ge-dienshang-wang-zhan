075498f9c13ccf3b17f123f520ba3533f3cc72e0
Yu-Song Syu <kuma@simbo.com.tw>
2020-11-05 23:51:46 +0800

    get product on creation of products page

--- src/components/pages/Products.vue
+++ src/components/pages/Products.vue
@@ -5,4 +5,25 @@
 </template>
 <script>
 
+    export default {
+        name: 'Products',
+        data() {
+            return {
+
+                products: []
+            }
+        },
+        methods: {
+            getProducts() {
+                this.$http.get('https://vue-course-api.hexschool.io/api/bearhsu2/products')
+                    .then((response) => {
+                        console.log(response.data.products)
+                    })
+            }
+        },
+        created() {
+            this.getProducts();
+        }
+    }
+
 </script>
