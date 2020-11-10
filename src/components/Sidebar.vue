9c7ce4e19c79864ef2a567834bb9a5e450049cad
Yu-Song Syu <kuma@simbo.com.tw>
2020-11-11 00:52:02 +0800

    整理 sidebar => 模擬訂單

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -4,7 +4,6 @@
 
         <nav class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse" id="sidebarMenu">
             <div class="sidebar-sticky pt-3">
-
                 <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
                     <span>管理員</span>
                     <a aria-label="Add a new report" class="d-flex align-items-center text-muted" href="#">
@@ -13,10 +12,18 @@
                 </h6>
                 <ul class="nav flex-column mb-2">
                     <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            <span data-feather="file-text"></span>
-                            <i class="fas fa-box-open"></i> 產品列表
-                        </a>
+                        <router-link class="nav-link" to="/admin/products">產品列表</router-link>
+                    </li>
+                </ul>
+                <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+                    <span>模擬功能</span>
+                    <a aria-label="Add a new report" class="d-flex align-items-center text-muted" href="#">
+                        <span data-feather="plus-circle"></span>
+                    </a>
+                </h6>
+                <ul class="nav flex-column mb-2">
+                    <li class="nav-item">
+                        <router-link class="nav-link disabled" to="/fake_path">模擬訂單</router-link>
                     </li>
                 </ul>
             </div>
