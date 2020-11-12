b6cda518a6867e1a74e4f35b472eca16f4bf3078
Yu-Song Syu <kuma@simbo.com.tw>
2020-11-12 14:57:20 +0800

    pagination vue

---
+++ src/components/Pagination.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <li :class="{'disabled':!pagination.has_pre}" class="page-item">
+                    <a @click.prevent="getProducts(pagination.current_page - 1)" aria-label="Previous" class="page-link"
+                       href="#">
+                        <span aria-hidden="true">&laquo;</span>
+                    </a>
+                </li>
+                <li :class="{'active': pagination.current_page === page}" :key="page" class="page-item"
+                    v-for="page in pagination.total_pages">
+                    <a @click.prevent="getProducts(page)" class="page-link" href="#">{{ page }}</a>
+                </li>
+                <li :class="{'disabled':!pagination.has_next}" class="page-item">
+                    <a @click.prevent="getProducts(pagination.current_page + 1)" aria-label="Next" class="page-link"
+                       href="#">
+                        <span aria-hidden="true">&raquo;</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Pagination",
+        props: {
+            pagination: {
+                type: Object,
+                required: true
+            }
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
