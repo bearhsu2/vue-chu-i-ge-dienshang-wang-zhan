59783df3aaad1a4ce7df8e8e78e72cc97f06c6a3
Yu-Song Syu <kuma@simbo.com.tw>
2020-11-12 17:30:07 +0800

    ignore unnecessary requests

--- src/App.vue
+++ src/App.vue
@@ -13,13 +13,13 @@
     export default {
         name: 'App',
         components: {},
-        created() {
-            const url = `${process.env.VUE_APP_SERVER_URL}/api/${process.env.VUE_APP_API_NAME}/products`;
-            this.$http.get(url)
-                .then((response) => {
-                    console.log(response.data.products)
-                })
-        }
+        // created() {
+        //     const url = `${process.env.VUE_APP_SERVER_URL}/api/${process.env.VUE_APP_API_NAME}/products`;
+        //     this.$http.get(url)
+        //         .then((response) => {
+        //             console.log(response.data.products)
+        //         })
+        // }
     }
 </script>
 
