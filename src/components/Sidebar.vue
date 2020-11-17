40abb6529fd50ce3aba88ff3cfa1c70d96b9208e
Yu-Song Syu <kuma@simbo.com.tw>
2020-11-18 01:19:19 +0800

    Add icon for product list

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -12,7 +12,9 @@
                 </h6>
                 <ul class="nav flex-column mb-2">
                     <li class="nav-item">
-                        <router-link class="nav-link" to="/admin/products">產品列表</router-link>
+                        <router-link class="nav-link" to="/admin/products">
+                            <i class="fas fa-box-open"></i> 產品列表
+                        </router-link>
                     </li>
                 </ul>
                 <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
