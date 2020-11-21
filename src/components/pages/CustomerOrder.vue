d8a513df22f1be915dd40f280b5d03b99cfd61d4
Yu-Song Syu <kuma@simbo.com.tw>
2020-11-21 17:18:48 +0800

    open modal

--- src/components/pages/CustomerOrder.vue
+++ src/components/pages/CustomerOrder.vue
@@ -23,7 +23,7 @@
                         </div>
                     </div>
                     <div class="card-footer d-flex">
-                        <button class="btn btn-outline-secondary btn-sm" type="button">
+                        <button @click="getProduct(item.id)" class="btn btn-outline-secondary btn-sm" type="button">
                             <i class="fas fa-spinner fa-spin"></i>
                             查看更多
                         </button>
@@ -42,10 +42,48 @@
             <loading :active.sync="isLoading"></loading>
         </div>
 
+        <div aria-hidden="true" aria-labelledby="productModalLabel" class="modal fade" id="productModal" role="dialog"
+             tabindex="-1">
+            <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="productModalLabel">產品詳情</h5>
+                    </div>
+
+
+                    <div class="container">
+
+                        <div :style="{backgroundImage: `url(${product.imageUrl})`}"
+                             style="height: 150px; background-size: cover; background-position: center">
+                        </div>
+                        <div class="mt-4">
+                            <span class="badge badge-secondary float-right ml-2">{{product.category}}</span>
+                            <h5 class="card-title">
+                                {{product.title}}
+                            </h5>
+                            <p class="card-text">{{product.description}}</p>
+                            <p class="card-text">{{product.content}}</p>
+                            <div class="d-flex justify-content-between align-items-baseline">
+                                <!-- <div class="h5">2,800 元</div> -->
+                                <div class="h5" v-if="!product.origin_price">售價{{product.price | currency}}元</div>
+                                <del class="h6" v-if="product.origin_price">原價{{product.origin_price | currency}}元</del>
+                                <div class="h5" v-if="product.origin_price">現在只要{{product.price | currency}}元</div>
+                            </div>
+                        </div>
+
+
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+
     </div>
 </template>
 <script>
 
+    import $ from 'jquery';
     import Pagination from '../Pagination';
 
 
@@ -55,6 +93,7 @@
         data() {
             return {
                 products: [],
+                product: {},
                 pagination: {},
                 isLoading: false,
             }
@@ -71,6 +110,20 @@
                         vm.isLoading = false;
                         vm.products = response.data.products;
                         vm.pagination = response.data.pagination;
+                    })
+            },
+            getProduct(id) {
+                const vm = this;
+
+                const url = `${process.env.VUE_APP_SERVER_URL}/api/${process.env.VUE_APP_API_NAME}/product/${id}`;
+                this.$http.get(url)
+                    .then((response) => {
+                        console.log(response);
+
+                        vm.product = response.data.product;
+                        $('#productModal').modal('show');
+
+
                     })
             }
         },
