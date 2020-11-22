810ffad67f1fc291f26c04374a0c3d0e83a66cbc
Yu-Song Syu <kuma@simbo.com.tw>
2020-11-22 20:54:03 +0800

    Coupons page: init

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Login from '@/components/pages/Login'
 import Dashboard from '@/components/Dashboard'
 import Products from '@/components/pages/Products'
+import Coupons from '@/components/pages/Coupons'
 
 Vue.use(VueRouter);
 
@@ -28,6 +29,15 @@ export default new VueRouter({
                     name: '產品頁',  // 元件的名稱
                     path: 'products',    // 元件的路徑
                     component: Products,  // 元件本身
+                    meta: {
+                        requiresAuth: true
+                    }
+
+                },
+                {
+                    name: '優惠碼頁',  // 元件的名稱
+                    path: 'coupons',    // 元件的路徑
+                    component: Coupons,  // 元件本身
                     meta: {
                         requiresAuth: true
                     }
